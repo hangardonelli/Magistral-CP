2fb763cda45e1dffe6f185da1905f13e824acc1e
hangardonelli <57008563+hangardonelli@users.noreply.github.com>
2021-06-10 20:30:12 -0300

    Update SP_AddToPenguinsThatHave.sql

--- src/Stored Procedures/SP_AddToPenguinsThatHave.sql
+++ src/Stored Procedures/SP_AddToPenguinsThatHave.sql
@@ -22,3 +22,31 @@ begin
     SELECT penguin_id, itemToAdd  FROM penguin_item WHERE item_id = neededItem; 
     commit;
 end;$$
+
+
+
+
+/*
+	
+	PURPOSE	  : Add an item to all penguins that have a specified item, secured version without preconditions
+	Parameters: 
+				neededItem	INT: The itemID necessary for the desired item to be added
+				itemtoAdd	INT: The item you want to add to all users who have the item 'neededItem'
+
+
+	==============================================================================================================
+	USAGE EXAMPLE: CALL SecuredAddToPenguinsThatHave(210, 1227)  
+	
+*/
+
+create or replace procedure SecuredAddToPenguinsThatHave(
+   neededItem int,
+   itemToAdd int
+)
+language plpgsql    
+as $$
+begin
+    INSERT INTO penguin_item(penguin_id, item_id)
+    SELECT penguin_id, itemToAdd  FROM penguin_item WHERE item_id = neededItem AND item_id <> itemToAdd; 
+    commit;
+end;$$
