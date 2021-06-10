677fd3a6a3aa149f51fc2d02e8dc7b0b72efd6b7
hangardonelli <57008563+hangardonelli@users.noreply.github.com>
2021-06-10 20:26:44 -0300

    Update SP_AddToPenguinsThatHave.sql

--- src/Stored Procedures/SP_AddToPenguinsThatHave.sql
+++ src/Stored Procedures/SP_AddToPenguinsThatHave.sql
@@ -4,6 +4,11 @@
 	Parameters: 
 				neededItem	INT: The itemID necessary for the desired item to be added
 				itemtoAdd	INT: The item you want to add to all users who have the item 'neededItem'
+
+	Precondition: There is no penguin in the penguin_item table that has neededItem and itemToAdd at the same time
+	==============================================================================================================
+	USAGE EXAMPLE: CALL AddToPenguinsThatHave(210, 1227)  
+	
 */
 
 create or replace procedure AddToPenguinsThatHave(
