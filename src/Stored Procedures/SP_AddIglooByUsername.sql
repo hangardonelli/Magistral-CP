b8b7d33d6dd3e461b796be3898657a5d4047382f
hangardonelli <57008563+hangardonelli@users.noreply.github.com>
2021-06-13 03:19:23 -0300

    Update SP_AddIglooByUsername.sql

--- src/Stored Procedures/SP_AddIglooByUsername.sql
+++ src/Stored Procedures/SP_AddIglooByUsername.sql
@@ -2,6 +2,16 @@ CREATE OR REPLACE PROCEDURE AddIglooByUsername(
    penguin_username varchar,
    igloo_id int
 )
+
+/*
+   Add an specified igloo to a specified penguin by his username.
+   Parameters:
+      -penguin_username:   The username (or nickname) of the penguin.
+      -igloo_id:           The specified id of the igloo to add to the player
+   
+   Precondition:
+      The user does not have the specified igloo
+*/
 language plpgsql    
 as $$
 begin
