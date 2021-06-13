cd33999ff9f76776538758776c2939ad320646cb
hangardonelli <57008563+hangardonelli@users.noreply.github.com>
2021-06-13 03:20:53 -0300

    Update SP_AddFurnitureByUsername.sql

--- src/Stored Procedures/SP_AddFurnitureByUsername.sql
+++ src/Stored Procedures/SP_AddFurnitureByUsername.sql
@@ -1,8 +1,14 @@
-```pgsql
+
 CREATE OR REPLACE PROCEDURE AddFurnitureByUsername(
    penguin_username varchar,
    furnitureID int
 )
+/*
+   Add a specified furniture to a specified player
+   Parameters:
+      -penguin_username:   The username (or nickname) of the player
+      -furnitureID         The ID of the furniture item that you want to add
+*/
 language plpgsql    
 as $$
 begin
@@ -10,4 +16,3 @@ begin
     SELECT id, furnitureID from penguin where username = lower(penguin_username);
     commit;
 end;$$
-```
